f829aa78ca328f29038c86563b0f661f1c2b406d
nanduvad <149459409+nanduvad@users.noreply.github.com>
2026-04-06 11:27:00 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from agent.agent_ppt import PPTAgent
-
-if __name__ == "__main__":
-    user_input = input("Enter topic: ")
-
-    agent = PPTAgent()
-    asyncio.run(agent.run(user_input))
